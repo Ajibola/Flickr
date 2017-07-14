fb8fcabd40a0f3db9ab29fc7ba4751bb7079293c
Ajibola Aiye <ajibolaaiyedogbon@yahoo.com>
2017-07-14 08:37:11 +0100

    added local photo cache

---
+++ data/src/main/java/android/app/data/local/PhotoCache.java
@@ -0,0 +1,21 @@
+package android.app.data.local;
+
+import android.app.data.entity.FlickrPhoto;
+
+import java.util.List;
+
+/**
+ * Created by hp on 7/14/2017.
+ */
+
+public class PhotoCache {
+
+    public void savePhotoSearchResults(String search, List<FlickrPhoto> photoList) {
+
+    }
+
+    public List<FlickrPhoto> getPhotoSearchResults(String search) {
+
+        return null;
+    }
+}
